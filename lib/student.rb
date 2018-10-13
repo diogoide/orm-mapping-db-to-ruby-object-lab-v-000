b4ed827de6b7e54a1d8312658dd38ab8b906e342
Diogo Ide <diogoide@gmail.com>
2018-10-13 21:03:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -112,5 +112,7 @@ class Student
     SQL
 
     # return a new instance of the Student class
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+      end
 end
